e4d1dc6ca7263d58ea597509e05946f493196296
Gulu <gulusv19999@gmail.com>
2022-09-30 20:14:26 +0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Home from "./components/Pages/Home/Home";
+import AppRouter from "./components/router/AppRouter";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      {/* <Home /> */}
+      <AppRouter />
+    </>
+  )}
+
+export default App
